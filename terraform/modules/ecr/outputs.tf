1a558405036e03b17088d032059877e30894f239
JoePearson93 <joe.pearson@live.com>
2026-04-29 11:46:16 +0100

    chore: included repo name in output.tf

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,3 +1,9 @@
 output "repository_url" {
   value = aws_ecr_repository.main.repository_url
 }
+
+output "repository_name" {
+  description = "ECR repository name"
+  value       = aws_ecr_repository.main.name
+  
+}
