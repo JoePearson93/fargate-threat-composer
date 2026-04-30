548030b8b52a0a9042f4ab1b5df3630d7feb5970
JoePearson93 <joe.pearson@live.com>
2026-04-30 15:24:54 +0100

    feat: add alb module with load balancer and target

---
+++ terraform/modules/alb/main.tf
@@ -0,0 +1,44 @@
+# Application load balancer
+
+resource "aws_lb" "name" {
+  name               = var.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = var.public_subnet_ids
+  security_groups    = var.alb_security_group_ids
+
+  enable_deletion_protection = false
+  
+  tags = {
+    Name = "${var.project_name}-alb"
+    environment = var.environment
+  }
+}
+
+# Target Group
+
+resource "aws_lb_target_group" "main" {
+  name        = "${var.project_name}-tg"
+  target_type = "ip"
+  port        = var.container_port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+
+  health_check {
+
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    protocol            = "HTTP"
+    matcher             = "200-399"
+
+  }
+}
+
+
+
+  
---
+++ terraform/modules/alb/variables.tf
@@ -0,0 +1,33 @@
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+}
+variable "alb_name" {
+  description = "Name of Application Load Balancer"
+  type        = string
+}
+
+variable "vpc_id"{
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "Pubic subnet IDs for the ALB"
+  type        = list(string)
+}
+
+variable "alb_security_group_ids" {
+  description = "Security group IDs for the ALB"
+  type        = list(string)
+}
+
+variable "environment" {
+  description = "The environment name (dev, staging, prod)"
+  type        = string
+}
+
+variable "container_port" {
+  description = "Port the container listens on"
+  type        = number
+}
