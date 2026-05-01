491754e5b0fce9f8c1368c2279bf2fea03827632
JoePearson93 <joe.pearson@live.com>
2026-05-01 11:41:12 +0100

    chore: modifications to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,39 @@
 module "vpc" {
   source = "./modules/vpc"
-  vpc_cidr             = var.vpc_cidr
-  public_subnet_cidrs  = var.public_subnet_cidrs
-  private_subnet_cidrs = var.private_subnet_cidrs
-  project_name         = var.project_name
-  environment          = var.environment
+  
+    vpc_cidr             = var.vpc_cidr
+    public_subnet_cidrs  = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    project_name         = var.project_name
+    environment          = var.environment
 }
 
 module "security_groups" {
   source                  = "./modules/security-groups"
-  vpc_id                  = module.vpc.vpc_id
-  ecs_port                = 8080
-  project_name            = var.project_name
-  environment             = var.environment
+  
+    vpc_id                  = module.vpc.vpc_id
+    ecs_port                = 8080
+    project_name            = var.project_name
+    environment             = var.environment
 }
 
 module "ecr" {
   source             = "./modules/ecr"
-  repository_name    = var.repository_name
-  project_name       = var.project_name
-  environment        = var.environment
+  
+    repository_name    = var.repository_name
+    project_name       = var.project_name
+    environment        = var.environment
 
 }
 
 module "alb" {
   source                  = "./modules/alb"
-  alb_name                = var.alb_name
-  vpc_id                  = module.vpc.vpc_id
-  public_subnet_ids       = module.vpc.public_subnet_ids
-  alb_security_group_ids  = [module.security_groups.alb_sg_id]
-  tg_name                 = var.tg_name
-  project_name            = var.project_name
-  environment             = var.environment
-  container_port          = var.container_port
+  
+    alb_name                = var.alb_name
+    vpc_id                  = module.vpc.vpc_id
+    public_subnet_ids       = module.vpc.public_subnet_ids
+    alb_security_group_ids  = [module.security_groups.alb_sg_id]
+    project_name            = var.project_name
+    environment             = var.environment
+    container_port          = var.container_port
 }
